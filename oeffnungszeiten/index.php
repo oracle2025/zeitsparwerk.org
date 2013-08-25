122161f8804b99709659fc0338b986ba89207613
Richard Spindler <richard@zsw.tv>
2013-08-25 17:49:36 +0200

    fixes

--- oeffnungszeiten/index.php
+++ oeffnungszeiten/index.php
@@ -10,7 +10,8 @@ menuOeffnungszeiten("");
 ?>
 				</div>
 				<div id="main" style="padding: 1.5em;">
-<h1 class="line874">Öffnungszeiten</h1>Dienstags von 18:00 bis 20:00 Uhr<br>Samstags von 15:00 bis 20:00 Uhr<br>sowie an den meisten Sonntagen zu Nachmittag und des öfteren Werktagen Abends.<br><br>Das Zeitsparwerk zu anderen Zeiten besichtigen? Kein Problem, das Werk öffnet die Tore gerne für Interessierte zu vorher vereinbarten Zeiten. Einfach eine kurze Mail an ein <a href="/?Verein:Vorstand">Vorstandsmitglied</a> und wir schauen, was sich machen lässt.<br><br>und natürlich zu <a href="/?Veranstaltungen%2C_Kurse_und_Workshops">Veranstaltungen</a>.<br>
+<h1 class="line874">Öffnungszeiten</h1>Dienstags von 18:00 bis 20:00 Uhr<br>Samstags von 15:00 bis 20:00 Uhr<br>sowie an den meisten Sonntagen zu Nachmittag und des öfteren Werktagen Abends.<br><br>Das Zeitsparwerk zu anderen Zeiten besichtigen? Kein Problem, das Werk öffnet die Tore gerne für Interessierte zu vorher vereinbarten Zeiten. Einfach eine kurze Mail an ein <a href="/verein/vorstand_mitglieder">Vorstandsmitglied</a> und wir schauen, was sich machen lässt.
+
 <br>
 
 				</div>
