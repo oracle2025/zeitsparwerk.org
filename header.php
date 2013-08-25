be389bbae055b05e2e2771e33240a0780b67f6af
Richard Spindler <richard.spindler@gmail.com>
2013-08-25 15:33:14 +0200

    new concept

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>zeitsparwerk.org</title>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.1/pure-min.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	</head>
+	<body style="background-color: #ffe647;">
+		<div class="pure-g-r">
+			<div class="pure-u-1-1">
+				<h1 style="font-size: 3em; letter-spacing: 0.2em; padding-left: 0.5em; margin: 0.3em; border: 0.3em solid red; font-family: Courier; text-transform: uppercase;">Zeitsparwerk<br><small>Hackerspace Wörgl</small></h1>
+			</div>
---
+++ menu.php
@@ -0,0 +1,90 @@
+<?
+
+function menuMain($page)
+{
+	$links = array(
+		"/" => "Zeitsparwerk",
+		"medien" => "Medien",
+		"projekte" => "Projekte",
+		"veranstaltungen_kurse_workshops" => "Veranstaltungen, Kurse und Workshops",
+		"verein" => "Verein",
+		"oeffnungszeiten" => "Öffnungszeiten",
+		"anfahrt" => "Anfahrt",
+		"kontakte" => "Kontakt",
+		"synergien_links" => "Synergien & Links",
+		"on_facebook" => "on Facebook"
+
+	);
+?>
+			<div class="pure-u-1-4">
+				<div style="border: 0.2em dashed black;">
+					<div class="pure-menu pure-menu-open" style="background-color: #ffe647; border: none;">
+						<ul>
+<?
+	foreach( $links as $href => $label ) {
+		if ($href=="/") { $href=""; }
+		if ($page == $href) {
+			echo '<li class="pure-menu-selected"><a href="/'.$href.'">'.$label.'</a></li>'."\n";
+		} else {
+			echo '<li><a href="/'.$href.'">'.$label.'</a></li>'."\n";
+		}
+	}
+?>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+<?
+}
+function menuZeitsparwerk($page)
+{
+?>
+	<div class="pure-menu pure-menu-open pure-menu-horizontal" style="background-color: #ffe647;">
+		<a href="#" class="pure-menu-heading">Zeitsparwerk</a>
+		<ul>
+			<li<? if($page=="grundriss") echo ' class="pure-menu-selected"';?>><a href="/grundriss">Grundriss</a></li>
+		</ul>
+	</div>
+<?
+}
+
+function menuMedien()
+{
+}
+
+
+function menuProjekte()
+{
+}
+
+function menuVeranstaltungenKurseUndWorkshops()
+{
+}
+
+function menuVerein()
+{
+}
+
+function menuOeffnungszeiten()
+{
+}
+
+function menuAnfahrt()
+{
+}
+
+function menuKontakt()
+{
+}
+
+function menuSynergienUndLinks()
+{
+}
+
+function menuOnFacebook()
+{
+}
+
+
+?>
