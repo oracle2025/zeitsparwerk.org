e1078f7a8d3f28e44c54a3db3e114e3c8182cdf3
Markus Sengthaler <m.sengthaler@gmail.com>
2013-09-05 16:40:38 +0200

    reformated

--- oeffnungszeiten/index.php
+++ oeffnungszeiten/index.php
@@ -3,22 +3,23 @@ require "../header.php";
 require "../menu.php";
 menuMain("oeffnungszeiten");
 ?>
-			<div class="pure-u-3-4">
-				<div style="border: 0.2em dashed black;">
+<div class="pure-u-3-4">
+	<div style="border: 0.2em dashed black;">
+		<?
+		menuOeffnungszeiten("");
+		?>
+	</div>
+	<div id="main" style="padding: 1.5em;">
+		<h1 class="line874">Öffnungszeiten</h1>
+		Dienstags von 18:00 bis 20:00 Uhr<br>
+		Samstags von 15:00 bis 20:00 Uhr<br>
+		sowie an den meisten Sonntagen zu Nachmittag und des öfteren Werktagen Abends.<br>
+		<br>
+		Das Zeitsparwerk zu anderen Zeiten besichtigen? Kein Problem, das Werk öffnet die Tore gerne für Interessierte zu vorher vereinbarten Zeiten. 
+		Einfach eine kurze Mail an ein <a href="/verein/vorstand_mitglieder">Vorstandsmitglied</a> und wir schauen, was sich machen lässt.
+		<br>
+	</div>
+</div>
 <?
-menuOeffnungszeiten("");
-?>
-				</div>
-				<div id="main" style="padding: 1.5em;">
-<h1 class="line874">Öffnungszeiten</h1>Dienstags von 18:00 bis 20:00 Uhr<br>Samstags von 15:00 bis 20:00 Uhr<br>sowie an den meisten Sonntagen zu Nachmittag und des öfteren Werktagen Abends.<br><br>Das Zeitsparwerk zu anderen Zeiten besichtigen? Kein Problem, das Werk öffnet die Tore gerne für Interessierte zu vorher vereinbarten Zeiten. Einfach eine kurze Mail an ein <a href="/verein/vorstand_mitglieder">Vorstandsmitglied</a> und wir schauen, was sich machen lässt.
-
-<br>
-
-				</div>
-			</div>
-
-
-<?
-
 require "../footer.php";
 ?>
