2a9a2c87ea5cd87dc7bd5d73e9be4e0e0847872c
Markus Sengthaler <m.sengthaler@gmail.com>
2013-09-05 16:55:44 +0200

    reformated medien

--- medien/index.php
+++ medien/index.php
@@ -3,28 +3,79 @@ require "../header.php";
 require "../menu.php";
 menuMain("medien");
 ?>
-			<div class="pure-u-3-4">
-				<div style="border: 0.2em dashed black;">
+<div class="pure-u-3-4">
+	<div style="border: 0.2em dashed black;">
+		<?
+		menuMedien("");
+		?>
+	</div>
+	<div id="main" style="padding: 1.5em;">
+		<h1>Medien</h1>
+		<h4>Bilder Gallerie<br></h4>
+		<iframe src="http://www.flickr.com/slideShow/index.gne?group_id=&amp;user_id=39016759@N06&amp;set_id=&amp;text=" frameborder="0" height="500" scrolling="no" width="500" align="center"></iframe><br>
+		<small>Created with 
+			<a href="http://www.admarket.se" title="Admarket.se">Admarket's</a> 
+			<a href="http://flickrslidr.com" title="flickrSLiDR">flickrSLiDR</a>.</small>&nbsp;
+			<p>Findet man auf dem <a href="http://www.flickr.com/photos/zeitsparwerk/">Zeitsparwerk Flickr Photo Stream</a></p>
+			<p>
+				<br>
+			</p>
+
+		<h4>Videos</h4>
+
+		<iframe width="560" height="315" src="//www.youtube.com/embed/CbKC5k6nU30" frameborder="0" allowfullscreen></iframe>
+		<p>
+			<a href="http://www.youtube.com/watch?v=CbKC5k6nU30">ZEITSPARWERK promotion video</a> from 
+			<a href="http://www.youtube.com/user/raimspi4">Raimund Spindler</a> on <a href="http://youtube.com">Youtube</a>.
+		</p>
+
+		<object height="270" width="480">
+			<param name="allowfullscreen" value="true">
+			<param name="allowscriptaccess" value="always">
+			<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=15966966&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0">
+			<embed src="http://vimeo.com/moogaloop.swf?clip_id=15966966&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480">
+		</object>
+		<p>
+			<a href="http://vimeo.com/15966966">Die Stimme der Energie - im Zeitsparwerk</a> from 
+			<a href="http://vimeo.com/mfuw">Albert Frisch</a> on 
+			<a href="http://vimeo.com">Vimeo</a>.
+		</p>
+		<object height="270" width="480">
+			<param name="allowfullscreen" value="true">
+			<param name="allowscriptaccess" value="always">
+			<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=15932080&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0">
+			<embed src="http://vimeo.com/moogaloop.swf?clip_id=15932080&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480">
+		</object>
+		<p>
+			<a href="http://vimeo.com/15932080">Club-Mate-Cola Dampfmaschine</a> from 
+			<a href="http://vimeo.com/mfuw">Albert Frisch</a> on <a href="http://vimeo.com">Vimeo</a>.
+		</p>
+		<object height="270" width="480">
+			<param name="allowfullscreen" value="true">
+			<param name="allowscriptaccess" value="always">
+			<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11971506&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1">
+			<embed src="http://vimeo.com/moogaloop.swf?clip_id=11971506&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480">
+		</object>
+		<p>
+			<a href="http://vimeo.com/11971506">Club-Mate Dampfmaschine</a> from 
+			<a href="http://vimeo.com/mfuw">Albert Frisch</a> on 
+			<a href="http://vimeo.com/">Vimeo</a>.
+		</p>
+		<object height="270" width="480">
+			<param name="allowfullscreen" value="true">
+			<param name="allowscriptaccess" value="always">
+			<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=5438646&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1">
+			<embed src="http://vimeo.com/moogaloop.swf?clip_id=5438646&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480">
+		</object>
+		<p>
+			<a href="http://vimeo.com/5438646">Zeitsparwerk - 360°</a> from 
+			<a href="http://vimeo.com/mfuw">Albert Frisch</a> on 
+			<a href="http://vimeo.com/">Vimeo</a>.
+		</p>
+
+	</div>
+</div>
+
 <?
-menuMedien("");
-?>
-				</div>
-				<div id="main" style="padding: 1.5em;">
-            <h1>Medien</h1>
-<h4>Bilder Gallerie<br></h4><iframe src="http://www.flickr.com/slideShow/index.gne?group_id=&amp;user_id=39016759@N06&amp;set_id=&amp;text=" frameborder="0" height="500" scrolling="no" width="500" align="center"></iframe><br><small>Created with <a href="http://www.admarket.se" title="Admarket.se">Admarket's</a> <a href="http://flickrslidr.com" title="flickrSLiDR">flickrSLiDR</a>.</small>&nbsp;<p>Findet man auf dem <a href="http://www.flickr.com/photos/zeitsparwerk/">Zeitsparwerk Flickr Photo Stream</a></p><p><br></p>
-<h4>Videos</h4>
-
-
-<iframe width="560" height="315" src="//www.youtube.com/embed/CbKC5k6nU30" frameborder="0" allowfullscreen></iframe>
-<p><a href="http://www.youtube.com/watch?v=CbKC5k6nU30">ZEITSPARWERK promotion video</a> from <a href="http://www.youtube.com/user/raimspi4">Raimund Spindler</a> on <a href="http://youtube.com">Youtube</a>.</p>
-
-<object height="270" width="480"><param name="allowfullscreen" value="true"><param name="allowscriptaccess" value="always"><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=15966966&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0"><embed src="http://vimeo.com/moogaloop.swf?clip_id=15966966&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480"></object><p><a href="http://vimeo.com/15966966">Die Stimme der Energie - im Zeitsparwerk</a> from <a href="http://vimeo.com/mfuw">Albert Frisch</a> on <a href="http://vimeo.com">Vimeo</a>.</p><object height="270" width="480"><param name="allowfullscreen" value="true"><param name="allowscriptaccess" value="always"><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=15932080&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0"><embed src="http://vimeo.com/moogaloop.swf?clip_id=15932080&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=000000&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480"></object><p><a href="http://vimeo.com/15932080">Club-Mate-Cola Dampfmaschine</a> from <a href="http://vimeo.com/mfuw">Albert Frisch</a> on <a href="http://vimeo.com">Vimeo</a>.</p><object height="270" width="480"><param name="allowfullscreen" value="true"><param name="allowscriptaccess" value="always"><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=11971506&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1"><embed src="http://vimeo.com/moogaloop.swf?clip_id=11971506&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480"></object><p><a href="http://vimeo.com/11971506">Club-Mate Dampfmaschine</a> from <a href="http://vimeo.com/mfuw">Albert Frisch</a> on <a href="http://vimeo.com/">Vimeo</a>.</p><object height="270" width="480"><param name="allowfullscreen" value="true"><param name="allowscriptaccess" value="always"><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=5438646&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1"><embed src="http://vimeo.com/moogaloop.swf?clip_id=5438646&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" height="270" width="480"></object><p><a href="http://vimeo.com/5438646">Zeitsparwerk - 360°</a> from <a href="http://vimeo.com/mfuw">Albert Frisch</a> on <a href="http://vimeo.com/">Vimeo</a>.</p>
-
-				</div>
-			</div>
-
-
-<?
-
 require "../footer.php";
 ?>
