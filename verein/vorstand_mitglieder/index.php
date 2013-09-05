ad10e632e66b6067a4c740f7b4d05e49feed6428
Markus Sengthaler <m.sengthaler@gmail.com>
2013-09-05 16:38:48 +0200

    updated most noteable persons

--- verein/vorstand_mitglieder/index.php
+++ verein/vorstand_mitglieder/index.php
@@ -22,8 +22,17 @@ menuMain("verein");
  		<span style="font-weight: bold;">Homepage: </span><a href="http://www.richardspindler.com/">www.richardspindler.com</a><br>
  		<span style="font-weight: bold;">Telefon:</span> +43 699 11 88 59 81<br>
 
+		<h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Lea Spindler</h4>
+		<span style="font-weight: bold; ">Status:</span>Schriftführer<br>
+
+		<h4 style="font-weight: bold;">Markus Sengthaler</h4>
+		<span style="font-weight: bold;">Status:</span> Kassier<br>
+		<span style="font-weight: bold;">Skills:</span> Programmierung, Fahrzeugtechnik, IT Technik<br>
+		<span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Herstellung_eines_Modellbaugetriebes">Herstellung eines Modellbaugetriebes</a>, Zahnradfräse inkl Härterofen, LED Nachrüstung eines RC cars<br>
+		<span style="font-weight: bold;">Beruf:</span> Programmierer (<a href="http://dynamind-toolbox.org/" target="_blank">dynamind-toolbox.org</a>), Student (Physik)<br>
+		<span style="font-weight: bold;">E-Mail:</span> <a href="mailto:m.sengthaler@gmail.com">m.sengthaler@gmail.com</a><br>
+
 		<h4 style="font-weight: bold;">Albert Frisch</h4>
-		<span style="font-weight: bold;">Status:</span> Schriftführer<br>
 		<span style="font-weight: bold;">Skills:</span> Elektronik, Programmierung, Ideenpool in Person<br>
 		<span style="font-weight: bold;">Projekte:</span> Arduino Workshop, Flammentornado<br>
 		<span style="font-weight: bold;">Beruf:</span> Doktoratsstudent (Physik)<br>
@@ -33,18 +42,12 @@ menuMain("verein");
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 
 		<h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Florian Decker</h4>
-		<span style="font-weight: bold; ">Status:</span>&nbsp;Kassier<br>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 
 		<h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Raimund Spindler</h4>
-		<span style="font-weight: bold;">Skills:</span> Metallverarbeitung, klassisches Handwerk<br>
-		<span style="font-weight: bold;">Projekte:</span> Kupferdampfmaschine<br>
+		<span style="font-weight: bold;">Skills:</span> Metallverarbeitung, klassisches Handwerk <br>
+		<span style="font-weight: bold;">Projekte:</span> Kupferdampfmaschine, Miniatur-Zeitsparwerk, Bollerwagen (inzwischen Wägen), zahlreiche Kunstobjekte<br>
 		<span style="font-weight: bold;">Beruf:</span> Spengler<br>
-
-		<h4 style="font-weight: bold;">Markus Sengthaler</h4>
-		<span style="font-weight: bold;">Skills:</span> Programmierung, Modellbau, Fahrzeugtechnik, Konstruktion<br>
-		<span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Herstellung_eines_Modellbaugetriebes">Herstellung eines Modellbaugetriebes</a>, Zahnradfräse inkl Härterofen, LED Nachrüstung eines RC cars<br>
-		<span style="font-weight: bold;">Beruf:</span> Student (Physik)<br>
 		<br>
 
 	</div>
