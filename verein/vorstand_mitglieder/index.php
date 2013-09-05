4fc2347569e76548eb12ae07d65420eead497737
Markus Sengthaler <m.sengthaler@gmail.com>
2013-09-05 16:29:17 +0200

    reformated

--- verein/vorstand_mitglieder/index.php
+++ verein/vorstand_mitglieder/index.php
@@ -3,19 +3,49 @@ require "../../header.php";
 require "../../menu.php";
 menuMain("verein");
 ?>
-			<div class="pure-u-3-4">
-				<div style="border: 0.2em dashed black;">
-<?
-menuVerein("vorstand_mitglieder");
-?>
-				</div>
-				<div id="main" style="padding: 1.5em;">
-  <h2>Vorstand &amp; Mitglieder</h2>Derzeit umfasst der Verein ca 15 ständige Mitglieder sowie eine Vielzahl von Stammbesuchern und Interessierten. Wir präsentieren hier eine Auswahl der most-notable persons:<br><br><h4 style="font-weight: bold;">Richard Spindler</h4><span style="font-weight: bold;">Status:</span> Obmann<br><span style="font-weight: bold;">Skills:</span> Management, Fitness, Programmierung<br><span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Fitness_%2F_Krafttraining">Fitnesstraining</a>, "Der Tisch"<br><span style="font-weight: bold;">Beruf:</span> Selbstständiger<br><span style="font-weight: bold;">E-Mail:</span> <a href="mailto:oracle@propirate.net">oracle@propirate.net</a><br><span style="font-weight: bold;">Homepage: </span><a href="http://www.richardspindler.com/">www.richardspindler.com</a><br><span style="font-weight: bold;">Telefon:</span>
-+43 699 11 88 59 81<br><h4 style="font-weight: bold;">Albert Frisch<br></h4><span style="font-weight: bold;">Status:</span> Schriftführer<br><span style="font-weight: bold;">Skills:</span> Elektronik, Programmierung, Ideenpool in Person<br><span style="font-weight: bold;">Projekte:</span> Arduino Workshop, Flammentornado<br><span style="font-weight: bold;">Beruf:</span> Doktoratsstudent (Physik)<br><span style="font-weight: bold;">E-Mail:</span> <a href="mailto:albert.frisch@gmail.com">albert.frisch@gmail.com</a><br><span style="font-weight: bold;">Blog:</span>
-<a href="http://wakalixes.propirate.net/" target="_blank">wakalixes.propirate.net</a><br><span style="font-weight: bold;">Telefon:</span> +43 664 7909103<br>
-<meta http-equiv="content-type" content="text/html; charset=utf-8"><h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Florian Decker<br></h4><span style="font-weight: bold; ">Status:</span>&nbsp;Kassier<span style="font-weight: bold; "><br></span><meta http-equiv="content-type" content="text/html; charset=utf-8"><h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Raimund Spindler</h4><div><span style="font-weight: bold;">Skills:</span> Metallverarbeitung, klassisches Handwerk<br><span style="font-weight: bold;">Projekte:</span> Kupferdampfmaschine<br><span style="font-weight: bold;">Beruf:</span> Spengler<br>
-<h4 style="font-weight: bold;">Markus Sengthaler<br></h4><span style="font-weight: bold;"></span><span style="font-weight: bold;">Skills:</span> Programmierung, Modellbau, Fahrzeugtechnik, Konstruktion<br> <span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Herstellung_eines_Modellbaugetriebes">Herstellung eines Modellbaugetriebes</a>, Zahnradfräse inkl Härterofen, LED Nachrüstung eines RC cars<br><span style="font-weight: bold;">Beruf:</span> Student (Physik)<br>
-<br>
+<div class="pure-u-3-4">
+	<div style="border: 0.2em dashed black;">
+		<?
+		menuVerein("vorstand_mitglieder");
+		?>
+	</div>
+	<div id="main" style="padding: 1.5em;">
+		<h2>Vorstand &amp; Mitglieder</h2>
+		Derzeit umfasst der Verein ca 15 ständige Mitglieder sowie eine Vielzahl von Stammbesuchern und Interessierten. Wir präsentieren hier eine Auswahl der most-notable persons:<br><br>
+
+ 		<h4 style="font-weight: bold;">Richard Spindler</h4>
+ 		<span style="font-weight: bold;">Status:</span> Obmann<br>
+ 		<span style="font-weight: bold;">Skills:</span> Management, Fitness, Programmierung<br>
+ 		<span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Fitness_%2F_Krafttraining">Fitnesstraining</a>, "Der Tisch"<br>
+ 		<span style="font-weight: bold;">Beruf:</span> Selbstständiger<br>
+ 		<span style="font-weight: bold;">E-Mail:</span> <a href="mailto:oracle@propirate.net">oracle@propirate.net</a><br>
+ 		<span style="font-weight: bold;">Homepage: </span><a href="http://www.richardspindler.com/">www.richardspindler.com</a><br>
+ 		<span style="font-weight: bold;">Telefon:</span> +43 699 11 88 59 81<br>
+
+		<h4 style="font-weight: bold;">Albert Frisch</h4>
+		<span style="font-weight: bold;">Status:</span> Schriftführer<br>
+		<span style="font-weight: bold;">Skills:</span> Elektronik, Programmierung, Ideenpool in Person<br>
+		<span style="font-weight: bold;">Projekte:</span> Arduino Workshop, Flammentornado<br>
+		<span style="font-weight: bold;">Beruf:</span> Doktoratsstudent (Physik)<br>
+		<span style="font-weight: bold;">E-Mail:</span> <a href="mailto:albert.frisch@gmail.com">albert.frisch@gmail.com</a><br>
+		<span style="font-weight: bold;">Blog:</span><a href="http://wakalixes.propirate.net/" target="_blank">wakalixes.propirate.net</a><br>
+		<span style="font-weight: bold;">Telefon:</span> +43 664 7909103<br>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+		<h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Florian Decker</h4>
+		<span style="font-weight: bold; ">Status:</span>&nbsp;Kassier<span style="font-weight: bold; "><br></span>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+		<h4 style="font-size: 12pt; font-weight: bold; margin-bottom: 6px; ">Raimund Spindler</h4>
+		<div><span style="font-weight: bold;">Skills:</span> Metallverarbeitung, klassisches Handwerk<br>
+			<span style="font-weight: bold;">Projekte:</span> Kupferdampfmaschine<br>
+			<span style="font-weight: bold;">Beruf:</span> Spengler<br>
+
+		<h4 style="font-weight: bold;">Markus Sengthaler</h4>
+		<span style="font-weight: bold;"></span><span style="font-weight: bold;">Skills:</span> Programmierung, Modellbau, Fahrzeugtechnik, Konstruktion<br>
+		<span style="font-weight: bold;">Projekte:</span> <a href="/?Projekte:Herstellung_eines_Modellbaugetriebes">Herstellung eines Modellbaugetriebes</a>, Zahnradfräse inkl Härterofen, LED Nachrüstung eines RC cars<br>
+		<span style="font-weight: bold;">Beruf:</span> Student (Physik)<br>
+		<br>
 </div>
 
 				</div>
