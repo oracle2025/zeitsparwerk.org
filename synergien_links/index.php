a5a1c7129a2aa51ad25511ffa646d794cb2b2ea0
Richard Spindler <richard.spindler@gmail.com>
2014-01-11 14:25:35 +0100

    amazon link

--- synergien_links/index.php
+++ synergien_links/index.php
@@ -10,7 +10,17 @@ menuSynergienLinks("");
 ?>
 				</div>
 				<div id="main" style="padding: 1.5em;">
- <h1>Synergien &amp; Links</h1><ul><li>IT-Syndikat <a href="http://www.it-syndikat.org/">www.it-syndikat.org</a></li><li>CPU - Computer Pool Unterland <a href="http://www.computerpool.at/">www.computerpool.at</a></li><li>THE BASE <a href="http://base.agitatio.org/">base.agitatio.org</a></li><li>rejected filmfestival <a href="http://www.filmfest-rejected.com/">filmfest-rejected.com</a></li></ul><p><br></p><ul><li><a href="http://metalab.at/">metalab.at</a></li><li><a href="http://www.ccc.de/">ccc.de</a><br></li></ul>
+ <h1>Synergien &amp; Links</h1><ul><li>IT-Syndikat <a href="http://www.it-syndikat.org/">www.it-syndikat.org</a></li>
+ 	<li>CPU - Computer Pool Unterland <a href="http://www.computerpool.at/">www.computerpool.at</a></li>
+ 	<li>THE BASE <a href="http://base.agitatio.org/">base.agitatio.org</a></li>
+ 	<li>rejected filmfestival <a href="http://www.filmfest-rejected.com/">filmfest-rejected.com</a></li></ul>
+ 	<p><br></p><ul><li><a href="http://metalab.at/">metalab.at</a></li>
+ 		<li><a href="http://www.ccc.de/">ccc.de</a><br></li></ul>
+ 		
+ 		
+ 		<h3>Amazon Partner Links:</h3>
+ 		<iframe src="http://rcm-eu.amazon-adsystem.com/e/cm?t=zeitsparwerk-21&o=3&p=8&l=as1&asins=B0086ODC3O&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+
 				</div>
 			</div>
 
