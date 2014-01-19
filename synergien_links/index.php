6f28b823777f568f938fc64c5071702deaa39320
Richard Spindler <richard.spindler@gmail.com>
2014-01-19 13:47:53 +0100

    links

--- synergien_links/index.php
+++ synergien_links/index.php
@@ -20,6 +20,8 @@ menuSynergienLinks("");
  		
  		<h3>Amazon Partner Links:</h3>
  		<iframe src="http://rcm-eu.amazon-adsystem.com/e/cm?t=zeitsparwerk-21&o=3&p=8&l=as1&asins=B0086ODC3O&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+<iframe src="http://rcm-eu.amazon-adsystem.com/e/cm?t=zeitsparwerk-21&o=3&p=8&l=as1&asins=B009GEC1G2&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+<iframe src="http://rcm-eu.amazon-adsystem.com/e/cm?t=zeitsparwerk-21&o=3&p=8&l=as1&asins=B00720TE1M&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 
 				</div>
 			</div>
